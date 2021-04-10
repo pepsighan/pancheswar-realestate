dd078982657dfd463e2b4b94053b5ae688d424ef
Sharad Chand <pepsighan@sharadchand.com>
2021-04-10 19:55:38 +0545

    Add a navigation with just the title

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+export default function Navigation() {
+  return (
+    <div className="p-4 fixed top-0 left-0 z-50 w-full">
+      <div className="w-2/3 mx-auto">
+        <h1 className="text-2xl font-bold">पञ्चेश्वर मल्टिपर्पोज एण्ड रियल स्टेट</h1>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,20 @@
-import Image from 'next/image';
+import Layout from 'components/Layout';
 import Meta from 'components/Meta';
+import Image from 'next/image';
 
 export default function Index() {
   return (
-    <>
+    <Layout>
       <Meta title="Pancheswar Multipurpose &amp; Realestate" description="Realestate Agents" />
 
-      <Image
-        src="/assets/guillaume-meurice-2739078.jpg"
-        alt="Photo by Guillaume Meurice"
-        layout="fill"
-      />
-    </>
+      <div className="min-h-screen overflow-hidden relative">
+        <Image
+          src="/assets/guillaume-meurice-2739078.jpg"
+          alt="Photo by Guillaume Meurice"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </Layout>
   );
 }
