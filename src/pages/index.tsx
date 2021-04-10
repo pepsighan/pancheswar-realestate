6550c7287cbd9c93f68ecc6662ca849bc1dfecac
Sharad Chand <pepsighan@sharadchand.com>
2021-04-10 18:49:32 +0545

    Add photo by guillame meurice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import Image from 'next/image';
+
 export default function Index() {
-  return <>Hello</>;
+  return (
+    <>
+      <Image
+        src="/assets/guillaume-meurice-2739078.jpg"
+        alt="Photo by Guillaume Meurice"
+        layout="fill"
+      />
+    </>
+  );
 }
