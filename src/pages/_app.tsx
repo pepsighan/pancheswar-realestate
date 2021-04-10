cd2606a3e0e383736c665b1d80a1cee59bd10ab0
Sharad Chand <pepsighan@sharadchand.com>
2021-04-10 09:12:04 +0545

    Clean up the code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,4 @@
-import "normalize.css";
-import { AppProps } from "next/app";
-// NOTE: Do not move the styles dir to the src.
-// They are used by the Netlify CMS preview feature.
-import "../../public/styles/global.css";
+import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
