4af827ac8a8c7775ca4601054ad4f6c9b2ae26e3
Sharad Chand <pepsighan@sharadchand.com>
2021-04-10 19:33:36 +0545

    Add meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import Image from 'next/image';
+import Meta from 'components/Meta';
 
 export default function Index() {
   return (
     <>
+      <Meta title="Pancheswar Multipurpose &amp; Realestate" description="Realestate Agents" />
+
       <Image
         src="/assets/guillaume-meurice-2739078.jpg"
         alt="Photo by Guillaume Meurice"
