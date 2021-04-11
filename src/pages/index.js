f540bd283947e342bb959c6930e861de13a7c95a
Sharad Chand <pepsighan@sharadchand.com>
2021-04-11 13:20:15 +0545

    Use the image cause next/image not possible in netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
 import { Box } from '@material-ui/core';
 import Layout from 'components/Layout';
 import Meta from 'components/Meta';
-import Image from 'next/image';
 
 export default function Index() {
   return (
     <Layout>
       <Meta title="Pancheswar Multipurpose &amp; Realestate" />
 
-      <Box sx={{ minHeight: '100vh', overflow: 'hidden', position: 'relative' }}>
-        <Image
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: '100vh',
+          overflow: 'hidden',
+        }}
+      >
+        <Box
+          component="img"
           src="/assets/guillaume-meurice-2739078.jpg"
           alt="Photo by Guillaume Meurice"
-          layout="fill"
-          objectFit="cover"
+          sx={{
+            width: '100%',
+          }}
         />
       </Box>
     </Layout>
